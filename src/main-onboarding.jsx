f6bc16a531cb4bce09c9dff428d301a4870d4c51
FaerGun <shved03@bk.ru>
2025-03-31 12:08:02 +0300

    верстка Onboarding 1

---
+++ src/main-onboarding.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Onboarding1 from './pages/onboarding-1'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Onboarding1 />
+  </React.StrictMode>,
+) 
